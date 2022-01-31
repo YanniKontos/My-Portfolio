9fc61500e8554a0bbcf7ad22acc3df4e43dcb13c
TheNarcissist1 <unreal810@gmail.com>
2022-01-31 22:49:25 +0000

    commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const navItems = document.getElementById('nav-items');
 const btn = document.getElementById('burger');
-const mainPage = document.getElementById('main-page');
 
 function showNavItems () {
     btn.classList.toggle('active');
