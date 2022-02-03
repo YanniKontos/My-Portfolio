599f06aefa67513e2d621d420bc272bb069cde9a
TheNarcissist1 <unreal810@gmail.com>
2022-02-03 21:08:54 +0000

    commit

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ function showNavItems () {
     btn.classList.toggle('active');
     navItems.classList.toggle('active');
 }
+
+function submitForm() {
+    alert('Thank You For Your Response, I will Get Back To You As Soon As Possible')
+}
