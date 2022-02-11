352f7015da6e7ecbdce83eb1d579e75ece8a1c6f
TheNarcissist1 <unreal810@gmail.com>
2022-02-11 23:24:38 +0000

    updated commit

--- app.js
+++ app.js
@@ -7,5 +7,16 @@ function showNavItems () {
 }
 
 function submitForm() {
-    alert('Thank You For Your Response, I will Get Back To You As Soon As Possible')
+    const emailInput = document.querySelector('.email-form').value;
+    const passwordInput = document.querySelector('.message-form').value;
+
+    if (emailInput == '' && passwordInput == '') {
+        alert('Please Fill Out The Form With Specified Data.');
+    }
+
+    else {
+        alert
+        ('Thank You For Filling Out The Form, I Will Return To You In Due Time.');
+    }
+
 }
